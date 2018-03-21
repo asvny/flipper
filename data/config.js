7bfdcbfa8e1ccb5e3bcf6a737cc7a2cc3f0294ff
asvny <annamalai.saro@gmail.com>
2018-03-21 12:04:00 +1100

    Update code

---
+++ data/config.js
@@ -0,0 +1,8 @@
+/**
+  The gameLevels has the config data for each level it is stored as nxn array
+  Here the indication is
+    0 - The light is turned on
+    1 - The light is turned off
+*/
+window.gameLevels =  {
+};
