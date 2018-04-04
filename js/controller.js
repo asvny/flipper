00ecfc773f2bb04de0e8080042fd92969b11e127
asvny <annamalai.saro@gmail.com>
2018-04-04 12:04:00 +1000

    Update code

---
+++ js/controller.js
@@ -0,0 +1,48 @@
+Flipper.Controller = {};
+
+/**
+  The controller is reponsible for showing/hiding screen states and acts as an interconnector
+  which connects the view and the model
+  The function is called each time when ever the hash is changed
+  The major functionalities
+    - Hide/Show the current screen based on hash
+    - Set the grid cells count as CSS variable for each level
+*/
+Flipper.Controller.handleEvent = (hash = Flipper.Model.currentHash) => {
+  let screen = hash.slice(1);
+  // Extract the level number from hash
+  let levelNumber = hash.match(/level-([0-9]+)/);
+
+  let { View } = Flipper;
+
+  // If levelNumber is present, start off with that level.
+  if (levelNumber != null) {
+    levelNumber = levelNumber[1];
+    screen = "level";
+
+    // Set the level in the model and set the grid board in the model.
+    Flipper.Model.currentLevel = levelNumber;
+    Flipper.Model.setBoard();
+    document.documentElement.style.setProperty(
+      "--Grid-size",
+      Flipper.Model.board[0].length
+    );
+  }
+
+  // If view is not present show an error message.
+  if (!View.hasOwnProperty(screen)) {
+    console.error(
+      `${screen} screen is missing from the available list of screens`
+    );
+  }
+
+  // If there is a previous screen ,hide it.
+  let currentScreen = $("[data-screen]");
+  if (currentScreen) {
+    Flipper.View.hide();
+  }
+
+  // Show the current screen and call the function responsible for event handling for that view
+  Flipper.View.show();
+  Flipper.View[screen]();
+};
