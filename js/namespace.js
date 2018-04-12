47b8a1b60965bb52bc9a39951b596f264e57e685
asvny <annamalai.saro@gmail.com>
2018-04-12 12:04:00 +1000

    Update code

---
+++ js/namespace.js
@@ -0,0 +1,9 @@
+// A namespace to group all the code in a single module;
+window.Flipper = window.Flipper || {};
+
+// It is the root element wrapper for the whole app and all screens
+Flipper.app = $("#App");
+
+Flipper.View = {};
+Flipper.Controller = {};
+Flipper.templates = {};
